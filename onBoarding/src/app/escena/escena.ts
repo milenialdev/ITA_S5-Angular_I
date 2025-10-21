73d858122db72d67ef392a0ce32b7c47c2a579b5
milenialdev <carlosbenitoandev@gmail.com>
2025-10-21 23:45:06 +0200

    developing level 3 (animation)

--- onBoarding/src/app/escena/escena.ts
+++ onBoarding/src/app/escena/escena.ts
@@ -12,15 +12,26 @@ export class Escena {
 
   currentStep = signal(0);
 
+  directionAnimation = signal<'left' | 'right'>('right');
+  animationTrigger = signal(0);
+
   nextStep(){
+    this.directionAnimation.set('right')
     this.currentStep.update(value => Math.min(value + 1, this.steps().length - 1));
   }
 
   previousStep(){
+    this.directionAnimation.set('left')
     this.currentStep.update(value => Math.max(value - 1, 0));
   }
 
   goToStep(index: number){
-    this.currentStep.set(index)
+    if(index !== this.currentStep()){
+      this.directionAnimation.set(index > this.currentStep() ? 'right' : 'left');
+      this.currentStep.set(index);
+    }
+  }
+  getAnimationClass(): string {
+    return this.directionAnimation() === 'right' ? 'slide-in-right' : 'slide-in-left';
   }
 }
