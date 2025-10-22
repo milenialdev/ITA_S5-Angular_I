8510bad5faaf75edd3dc241060f590a58216079a
milenialdev <carlosbenitoandev@gmail.com>
2025-10-22 13:05:57 +0200

    starting testing, commiting before editing configuration due to error

--- onBoarding/src/app/services/steps.spec.ts
+++ onBoarding/src/app/services/steps.spec.ts
@@ -13,4 +13,27 @@ describe('Steps', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return array with specific length', () => {
+    const data = service.getSteps();
+    expect(data.length).toBe(3);
+  });
+
+  it('should have correct structure', () => {
+    const data = service.getSteps();
+    expect(data[0].bgcolor).toBeDefined();
+    expect(data[0].img).toBeDefined();
+    expect(data[0].title).toBeDefined();
+    expect(data[0].description).toBeDefined();
+    expect(typeof data[0].bgcolor).toBe('string');
+    expect(typeof data[0].img).toBe('string');
+    expect(typeof data[0].title).toBe('string');
+    expect(typeof data[0].description).toBe('string');
+  });
+
+  it('should have correct bgcolor for first step', () => {
+  const data = service.getSteps();
+  expect(data[0].bgcolor).toBe('#4ca2a8');
+  });
+
 });
